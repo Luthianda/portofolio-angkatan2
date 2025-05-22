ba6fed9a60261fc3c90db865979db1353d268e30
Luthianda <luthianda17@gmail.com>
2025-05-22 15:48:38 +0700

    first commit

--- admin/user.php
+++ admin/user.php
@@ -1,3 +1,18 @@
+<?php
+include 'config/koneksi.php';
+
+// munculkan/pilih semua data dari table user urutkan dari yang terbesar
+// ke terkecil
+
+$query = mysqli_query($config, "SELECT * FROM users ORDER BY id DESC");
+$row = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+    $queryDelete = mysqli_query($config, "DELETE FROM users WHERE id='$id'");
+    header("location:user.php?hapus=berhasil");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,37 +24,7 @@
 </head>
 <body>
     <div class="wrapper">
-        <header class="shadow">
-            <nav class="navbar navbar-expand-lg bg-body-white">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">CMS Ananda</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Page
-                                    </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr class="dropdown-divider"></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="user.php">User</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
+        <?php include 'inc/header.php'; ?>
         <div class="content">
             <div class="container mt-5">
                 <div class="row">
@@ -63,7 +48,7 @@
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
+                                            <!-- <tr>
                                                 <td>1</td>
                                                 <td>Ananda</td>
                                                 <td>luthianda17@gmail.com</td>
@@ -72,7 +57,21 @@
                                                     <a onclick="return confirm ('Are you sure?')" 
                                                     href="" class="btn btn-warning btn-sm">Delete</a>
                                                 </td>
-                                            </tr>
+                                            </tr> -->
+                                            <?php $i = 1;
+                                            foreach ($row as $key => $data): ?>
+                                                <tr>
+                                                    <!-- <td><?= $i++ ?></td> -->
+                                                    <td><?= $key + 1 ?></td>
+                                                    <td><?= $data['name'] ?></td>
+                                                    <td><?= $data['email'] ?></td>
+                                                    <td>
+                                                        <a href="tambah-user.php?edit=<?php echo $data['id'] ?>" class="btn btn-success btn-sm">Edit</a>
+                                                        <a onclick="return confirm('Are you sure??')"
+                                                            href="user.php?delete=<?php echo $data['id'] ?>" class="btn btn-warning btn-sm">Delete</a>
+                                                    </td>
+                                                </tr>
+                                            <?php endforeach ?>
                                         </tbody>
                                     </table>
                                 </div>
